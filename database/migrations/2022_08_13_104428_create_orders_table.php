f183ee461ab72fae655af503ea5a73780750beb9
Claudia1010 <claudiachristensen1@gmail.com>
2022-08-13 13:01:29 +0200

    feat: added orders table

---
+++ database/migrations/2022_08_13_104428_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('payment_method_id');
+            $table->unsignedBigInteger('carrier_id');
+            $table->double('ammount');
+            $table->date('order_date');
+            $table->string('order_email')->nullable();
+            $table->string('order_address')->nullable();
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('payment_method_id')->references('id')->on('payment_methods')->onDelete('cascade');
+            $table->foreign('carrier_id')->references('id')->on('carriers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
